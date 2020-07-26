1a71522ec0fd3417f7cf4a59fb779616aef05b58
alexjeman <contact@alexjeman.com>
2020-07-26 11:56:36 +0300

    Add GET by id and POST endpoints

--- src/main/java/com/example/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/restfulapi/controller/EmployeeController.java
@@ -1,28 +1,43 @@
 package com.example.restfulapi.controller;
 
+import com.example.restfulapi.exception.ResourceNotFoundException;
 import com.example.restfulapi.model.Employee;
 import com.example.restfulapi.repository.EmployeeRepository;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1")
-public class EmployeeController {
+class EmployeeController {
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
+    private final EmployeeRepository repository;
+
+    EmployeeController(EmployeeRepository repository) {
+        this.repository = repository;
+    }
 
     // GET employees
 
-    @GetMapping(path = "/employees")
     @ApiOperation(value = "Get all employees", notes = "Get a list of all employees", response = Employee.class)
-    public List<Employee> getAllEmployees() {
-        return this.employeeRepository.findAll();
+    @GetMapping("/employees")
+    List<Employee> all() {
+        return repository.findAll();
     }
 
+
+    @ApiOperation(value = "Get employee by ID", notes = "Get a list of all employees", response = Employee.class)
+    @GetMapping("/employees/{id}")
+    Employee one(@PathVariable Long id) throws ResourceNotFoundException {
+
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Resource not found by this id"));
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@Validated @RequestBody Employee employee) {
+        return repository.save(employee);
+    }
 }
